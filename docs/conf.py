baca80c16360f98fbd4c8ace9dc08e77a1d58ecd
Matthew Ballance <matt.ballance@gmail.com>
2026-06-18 14:19:19 +0000

    Add root ivpm.yaml
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,10 @@ release = "0.1"
 
 extensions = ["myst_parser"]
 
+# Copy the top-level package catalog verbatim into the site root so it is
+# served at https://edapack.github.io/ivpm.yaml
+html_extra_path = ["ivpm.yaml"]
+
 html_theme = "alabaster"
 html_title = "EDAPack"
 
